9e83d30e86aaca81c987d3796dd4a6d5938e52b7
carloc3005 <carlo89512@yahoo.com>
2025-04-12 09:07:17 -0700

    creating main page and added router-dom

---
+++ src/frontend/pages/home.jsx
@@ -0,0 +1,13 @@
+// home.jsx
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>This is the Home Page</h1>
+      <p>Welcome to the main page!</p>
+    </div>
+  )
+}
+
+export default Home
