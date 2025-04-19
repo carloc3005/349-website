f0bd54618fe3c876e5ca73477dd7f203bd28246b
carloc3005 <carlo89512@yahoo.com>
2025-04-19 09:55:13 -0700

    added service page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './frontend/pages/home.jsx' 
 import About from './frontend/pages/about.jsx'
+import Service from './frontend/pages/service.jsx'
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
 
         {/* Example: Add additional routes here as needed */}
         {/* <Route path="/about" element={<About />} /> */}
-        <Route path="/" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/frontend/pages/service.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Service() {
+  return (
+    <div>
+      <h1>This is the Service Page</h1>
+      <p>Welcome to the main page!</p>
+    </div>
+  )
+}
+
+export default Service
