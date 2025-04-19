4d0c72cffb1965c7bb8a19f79b1abde399e1fd1d
carloc3005 <carlo89512@yahoo.com>
2025-04-19 10:51:10 -0700

    added tailwindcss

--- src/frontend/pages/home.jsx
+++ src/frontend/pages/home.jsx
@@ -1,13 +1,16 @@
-// home.jsx
-import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Home() {
   return (
     <div>
-      <h1>This is the Home Page</h1>
-      <p>Welcome to the main page!</p>
+
     </div>
-  )
+
+
+  );
 }
 
-export default Home
+export default Home;
