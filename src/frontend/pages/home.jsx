1740077d474485fa66c8d2a5ff0e6dfdf40959a2
carloc3005 <carlo89512@yahoo.com>
2025-04-19 17:34:26 -0700

    added tailwindcss

--- src/frontend/pages/home.jsx
+++ src/frontend/pages/home.jsx
@@ -1,3 +1,4 @@
+import React from 'react'; // Add this import
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,7 +7,10 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function Home() {
   return (
     <div>
-
+      {/* Test element for Tailwind */}
+      <div className="bg-blue-500 text-white p-4 rounded">
+        Tailwind Test Element
+      </div>
     </div>
 
 
